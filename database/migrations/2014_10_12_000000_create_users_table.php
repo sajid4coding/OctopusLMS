f98855c5edca838df459c716aa5fdd532a24078d
Avijit Karmoker <ovijit1hsc@gmail.com>
2023-06-01 00:50:19 +0600

    WIP: authentication adding facebook, gmail etc

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('full name');
+            $table->string('user name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default('admin');
             $table->rememberToken();
             $table->timestamps();
         });
